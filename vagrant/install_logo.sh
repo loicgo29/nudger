c4561eab3067911cd70b1dc6110712f731239ab4
loicgo29 <154337066+loicgo29@users.noreply.github.com>
2025-08-07 17:34:55 +0200

    v2 vangrant basique et 3 serveurs

---
+++ vagrant/install_logo.sh
@@ -0,0 +1,70 @@
+#!/bin/zsh
+
+###############################################################
+#  TITRE: 
+#
+#  AUTEUR:   Loic G.
+#  VERSION: 
+#  CREATION:  
+#  MODIFIE: 
+#
+#  DESCRIPTION: 
+###############################################################
+
+
+
+# Variables ###################################################
+
+
+
+# Functions ###################################################
+
+
+
+# Let's Go !! #################################################
+
+echo "
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=100000
+HISTFILESIZE=20000
+
+alias ll='ls -laFh --color=auto'
+alias la='ls -A'
+alias l='ls -larth'
+alias gl='git log'
+alias gst='git status'
+alias gg='git log --oneline --all --graph --name-status'
+alias s='sudo -s'
+alias h='helm'
+alias k='kubectl'
+alias kcc='kubectl config current-context'
+alias kg='kubectl get'
+alias kga='kubectl get all --all-namespaces'
+alias kgp='kubectl get pods'
+alias kgs='kubectl get services'
+alias kn='kubens'
+alias ksgp='kubectl get pods -n kube-system'
+alias kss='kubectl get services -n kube-system'
+alias kuc='kubectl config use-context'
+alias kx='kubectx'
+
+
+
+clear
+echo -e '\033[0;32m
+██╗░░░░░░█████╗░░██████╗░░█████╗░
+██║░░░░░██╔══██╗██╔════╝░██╔══██╗
+██║░░░░░██║░░██║██║░░██╗░██║░░██║
+██║░░░░░██║░░██║██║░░╚██╗██║░░██║
+███████╗╚█████╔╝╚██████╔╝╚█████╔╝
+╚══════╝░╚════╝░░╚═════╝░░╚════╝░
+'
+export KUBECONFIG=/home/vagrant/.k0s/kubeconfig
+" >> /home/vagrant/.bashrc
