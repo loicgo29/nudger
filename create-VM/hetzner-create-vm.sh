d95c8ad67cf8fe54b76ac0abf17d4b1ac4bd1ecc
loicgo29 <154337066+loicgo29@users.noreply.github.com>
2025-08-21 19:32:03 +0200

    cliud init hetzner

---
+++ create-VM/hetzner-create-vm.sh
@@ -0,0 +1,8 @@
+#hcloud ssh-key create --name loic-vm-key --public-key "$(cat ~/.ssh/id_vm_ed25519.pub)"
+
+hcloud server create \
+  --name nudger-vm-k8s \
+  --image ubuntu-22.04 \
+  --type cpx21 \
+  --user-data-from-file ./cloud-init.yaml 
+
