eebbfdb7a3d7ecaaa6f0bab1f31d065c939b3f7f
loicgo29 <154337066+loicgo29@users.noreply.github.com>
2025-08-21 19:34:45 +0200

    clean

--- create-VM/hetzner-create-vm.sh
+++ create-VM/hetzner-create-vm.sh
@@ -1,4 +1,4 @@
-#hcloud ssh-key create --name loic-vm-key --public-key "$(cat ~/.ssh/id_vm_ed25519.pub)"
+hcloud ssh-key create --name loic-vm-key --public-key "$(cat ~/.ssh/id_vm_ed25519.pub)"
 
 hcloud server create \
   --name nudger-vm-k8s \
