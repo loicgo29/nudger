2591820218d4e4313e6c4bdc8e7052601ab9a9eb
Loïc Gourmelon <loicgourmelon@gmail.com>
2025-08-24 10:24:52 +0200

    major

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ DEPOT_GIT="https://github.com/loicgo29/nudger.git"
 ID_SSH="id_vm_ed25519"
 
 # 1️⃣ Créer la VM
-IP=$(./create-VM/vps/create-vm.sh "$VM_NAME" "$USER" "$DEPOT_GIT" | awk '/VM IP:/ {print $3}')
+IP=$(./create-VM/vps/create-vm.sh "$VM_NAME" "$USER" "$DEPOT_GIT"  | tee /dev/tty | awk '/VM IP:/ {print $3}')
 
 # 2️⃣ Mettre à jour l’inventaire
 cat > inventory.ini <<EOF
@@ -16,6 +16,6 @@ $VM_NAME ansible_host=$IP ansible_user=$USER ansible_ssh_private_key_file=$HOME/
 EOF
 
 # 3️⃣ Lancer Ansible
-ansible-playbook -i infra/k8s-ansible/inventory.ini infra/k8s-ansible/playbooks/infra-devops.yml
-ansible-playbook -i infra/k8s-ansible/inventory.ini infra/k8s-ansible/playbooks/kubernetes-setup.yml
+cd infra/k8s-ansible
+ansible-playbook -i ./inventory.ini ./playbooks/nudger.yml
 
